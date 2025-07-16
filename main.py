6c44da2778e8d153845a3c20bd84118377b204b3
Dor Bar <dorbar424@gmail.comgit config --global user.name Dor>
2025-07-16 21:02:20 +0300

    Revert "blue btn new file1"
    This reverts commit 3ae4982990c3e741f81ab8ce15bae20d8d1562c9.

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from datetime import datetime
-
 def main():
     print("Welcome to Git Assignment")
-    now = datetime.now()
-    print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == "__main__":
     main()
