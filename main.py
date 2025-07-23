c10f9f057787a1f5e92bad735311c4627f6470a1
Dor Bar <dorbar424@gmail.comgit config --global user.name Dor>
2025-07-23 20:46:14 +0300

    Print date and name

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 def main():
     print("Welcome to Git Assignment")
+    now = datetime.now()
+    print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
     print("My Name Is Dor")
 
 if __name__ == "__main__":
